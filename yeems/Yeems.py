b260862e465ef69dd67b31b025adbf477a964cba
RedLine18 <RedLine18@users.noreply.github.com>
2020-07-09 00:12:03 +0600

    Added Image swapping, broke colourly wholourly

--- yeems/Yeems.py
+++ yeems/Yeems.py
@@ -1,40 +1,62 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import colorchooser
-from PIL import ImageTk,Image
+import itertools
+from PIL import ImageTk, Image
+
+win = tk.Tk()
+win.geometry('800x1000')
+win.resizable(0, 0)
+
+panel = tk.Label(win)
+panel.pack()
+
+testimages = ["../Resources/Untitled.png", "../Resources/Human Male 1.png"]
+testimages = itertools.cycle(testimages)
+
+
+def next_img():
+    try:
+        img = next(testimages)
+    except StopIteration:
+        return
+
+    img = Image.open(img)
+    img = ImageTk.PhotoImage(img)
+    panel.img = img
+    panel['image'] = img
 
 def change_img_colour(img, colour, pal_size=64):
-    index_img = img.convert('RGBA').convert(mode='P', dither='NONE', colors = pal_size)
+    index_img = img.convert('RGBA').convert(mode='P', dither='NONE', colors=pal_size)
     palette = index_img.getpalette()
     for i in range(pal_size):
         i = i * 3
 
         r_index = i
-        g_index = i+1
-        b_index = i+2
+        g_index = i + 1
+        b_index = i + 2
 
-        palette[r_index] = int(palette[r_index] - (palette[r_index] - colour[0])/2)
-        palette[g_index] = int(palette[g_index] - (palette[g_index] - colour[1])/2)
-        palette[b_index] = int(palette[b_index] - (palette[b_index] - colour[2])/2)
+        palette[r_index] = int(palette[r_index] - (palette[r_index] - colour[0]) / 2)
+        palette[g_index] = int(palette[g_index] - (palette[g_index] - colour[1]) / 2)
+        palette[b_index] = int(palette[b_index] - (palette[b_index] - colour[2]) / 2)
     index_img.putpalette(palette)
     return index_img
-
-root = Tk()
-
-img = ImageTk.PhotoImage(Image.open("../Resources/Untitled.png"))
 
 def colour_wolour():
     global img
     clr = colorchooser.askcolor(title="color wolour")
-    img = ImageTk.PhotoImage(change_img_colour(Image.open("../Resources/Untitled.png"), clr[0]))
+    img = ImageTk.PhotoImage(change_img_colour(Image.open(testimages), clr[0]))
 
-change_colour = Button(root, text="Change colour", command=colour_wolour)
+change_colour = Button(text="Change colour", command=colour_wolour)
 change_colour.pack()
-
-canvas = Canvas(root, width = 1000, height = 1000)
-canvas.pack()
+nextImageButton = tk.Button(text='Next image', command=next_img).place(x=10, y=10)
 
 
-while True:
-    canvas.create_image(20, 20, anchor=NW, image=img)
-    root.update()
-    root.update_idletasks()
+
+next_img()
+win.mainloop()
+
+
+
+
+
