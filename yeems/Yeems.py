af7930a7c136ab55bcd0df915c4d42c2039ee3cd
Zayyan Abbas <zayyanabbas@pop-os.localdomain>
2020-07-09 15:15:31 +0600

    Fixed colourly-wholourly

--- yeems/Yeems.py
+++ yeems/Yeems.py
@@ -11,20 +11,20 @@ win.resizable(0, 0)
 panel = tk.Label(win)
 panel.pack()
 
-testimages = ["../Resources/Untitled.png", "../Resources/Human Male 1.png"]
-testimages = itertools.cycle(testimages)
+img_index = 0
 
+img_paths = ["../Resources/Untitled.png", "../Resources/Human Male 1.png"]
+imgs = []
+for i in img_paths:
+    imgs.append(ImageTk.PhotoImage(Image.open(i)))
 
 def next_img():
-    try:
-        img = next(testimages)
-    except StopIteration:
-        return
-
-    img = Image.open(img)
-    img = ImageTk.PhotoImage(img)
-    panel.img = img
-    panel['image'] = img
+    global img_index
+    img_index = img_index + 1
+    if img_index > len(img_paths)-1:
+        img_index = 0
+    img = ImageTk.PhotoImage(Image.open(img_paths[img_index]))
+    panel['image'] = imgs[img_index]
 
 def change_img_colour(img, colour, pal_size=64):
     index_img = img.convert('RGBA').convert(mode='P', dither='NONE', colors=pal_size)
@@ -43,20 +43,16 @@ def change_img_colour(img, colour, pal_size=64):
     return index_img
 
 def colour_wolour():
-    global img
     clr = colorchooser.askcolor(title="color wolour")
-    img = ImageTk.PhotoImage(change_img_colour(Image.open(testimages), clr[0]))
+    imgs[img_index] = ImageTk.PhotoImage(change_img_colour(Image.open(img_paths[img_index]), clr[0]))
+    panel['image'] = imgs[img_index]
 
 change_colour = Button(text="Change colour", command=colour_wolour)
 change_colour.pack()
 nextImageButton = tk.Button(text='Next image', command=next_img).place(x=10, y=10)
 
-
-
 next_img()
-win.mainloop()
 
-
-
-
-
+while True:
+    win.update()
+    win.update_idletasks()
