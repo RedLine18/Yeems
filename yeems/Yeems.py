3d2ba71186c048ab2272a118bd3f34187ee9bbbb
RedLine18 <RedLine18@users.noreply.github.com>
2020-07-10 13:36:36 +0600

    Replaced test images with actual images

--- yeems/Yeems.py
+++ yeems/Yeems.py
@@ -8,18 +8,19 @@ win = tk.Tk()
 win.geometry('800x900')
 win.resizable(0, 0)
 
-canvas = Canvas(win, width = 500, height = 500)
+canvas = Canvas(win, width=500, height=500)
 canvas.pack()
 
 img_index = 0
 
-img_paths = ["../Resources/Untitled.png", "../Resources/Human Male 1.png", "../Resources/yeems1.png", "../Resources/yeems3.jpg", "../Resources/yeems5.png", "../Resources/yeems7.jpg", "../Resources/yeems9.png"]
+img_paths = ["../Resources/hair1.png", "../Resources/base1.png", "../Resources/eyebrows1.png", "../Resources/eyes1.png",
+             "../Resources/nose1.png", "../Resources/mouth1.png"]
 
 color = []
 for i in img_paths:
-    color.append([0,0,0])
+    color.append([0, 0, 0])
 
-slot_options = [1,1,1,1,1,1,1]
+slot_options = [1, 1, 1, 1, 1, 1, 1]
 
 vhair = 0
 vhead_shape = 1
@@ -29,32 +30,45 @@ vnose = 4
 vmouth = 5
 vfacial_acc = 6
 
-slots = [["../Resources/Human Male Ranger.jpg", "../Resources/Untitled.png"], ["../Resources/Air Elemental Myrmidon.jpg", "../Resources/Human Male 1.png"], 
-["../Resources/yeems1.png", "../Resources/yeems2.jpg"], ["../Resources/yeems3.jpg", "../Resources/yeems4.jpg"], ["../Resources/yeems5.png", "../Resources/yeems6.jpg"],
-["../Resources/yeems7.jpg", "../Resources/yeems8.jpg"], ["../Resources/yeems9.png", "../Resources/yeems10.jpg"]]
+slots = [["../Resources/hair1.png"], ["../Resources/base1.png"],
+         ["../Resources/eyebrows1.png"], ["../Resources/eyes1.png"], ["../Resources/nose1.png"],
+         ["../Resources/mouth1.png"]]
 
 imgs = []
 for i in img_paths:
     imgs.append(ImageTk.PhotoImage(Image.open(i)))
 
+
 def hair():
     global img_index
     img_index = vhair
+
+
 def head_shape():
     global img_index
     img_index = vhead_shape
+
+
 def eyebrows():
     global img_index
     img_index = veyebrows
+
+
 def eyes():
     global img_index
     img_index = veyes
+
+
 def nose():
     global img_index
     img_index = vnose
+
+
 def mouth():
     global img_index
     img_index = vmouth
+
+
 def facial_acc():
     global img_index
     img_index = vfacial_acc
@@ -63,20 +77,22 @@ def facial_acc():
 def next_option():
     global slot_options
     slot_options[img_index] = slot_options[img_index] + 1
-    if slot_options[img_index] > len(slots[img_index])-1:
+    if slot_options[img_index] > len(slots[img_index]) - 1:
         slot_options[img_index] = 0
     img_paths[img_index] = slots[img_index][slot_options[img_index]]
     imgs[img_index] = ImageTk.PhotoImage(Image.open(img_paths[img_index]))
+
 
 def prev_option():
     global slot_options
     slot_options[img_index] = slot_options[img_index] - 1
     if slot_options[img_index] < 0:
-        slot_options[img_index] = len(slots[img_index])-1
+        slot_options[img_index] = len(slots[img_index]) - 1
     img_paths[img_index] = slots[img_index][slot_options[img_index]]
     imgs[img_index] = ImageTk.PhotoImage(Image.open(img_paths[img_index]))
 
     imgs[img_index] = ImageTk.PhotoImage(change_img_colour(Image.open(img_paths[img_index]), color[img_index]))
+
 
 def change_img_colour(img, colour, pal_size=64):
     index_img = img.convert('RGBA').convert(mode='P', dither='NONE', colors=pal_size)
@@ -94,10 +110,12 @@ def change_img_colour(img, colour, pal_size=64):
     index_img.putpalette(palette)
     return index_img
 
+
 def colour_wolour():
     clr = colorchooser.askcolor(title="color wolour")
     imgs[img_index] = ImageTk.PhotoImage(change_img_colour(Image.open(img_paths[img_index]), clr[0]))
     color[img_index] = clr[0]
+
 
 change_colour = Button(text="Change colour", command=colour_wolour)
 change_colour.pack()
@@ -115,6 +133,8 @@ next_option()
 
 while True:
     for i in imgs:
+        im = Image.open(i)
+
         canvas.create_image(50, 100, anchor=NW, image=i)
 
     win.update()
